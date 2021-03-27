3e1bdb3268f5d00e989d15f63f88e76b139987ca
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-27 16:47:02 -0300

    bubble sort terminado

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* selecciona y transforma en variable */
-let number = document.querySelector('#number');
-let btnDraw = document.querySelector('#draw');
-let btnSort = document.querySelector('#sort');
+let btnDraw = document.querySelector('#btnDraw');
+let btnSort = document.querySelector('#btnSort');
 
 /* variables y sus valores */
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -9,28 +8,18 @@ let suits = ['♦', '♥', '♠', '♣'];
 /* almacenador */
 let orderCards = [];
 
-/* inversor de valores de cartas A, J, Q, K */
-function changeValue(valor) {
-    switch (valor) {
-        case 1: return "A";
-        case 11: return "J";
-        case 12: return "Q";
-        case 13: return "K";
-        default: return valor;
-    }
-}
 
 /* creador de cartas */
 function createCards(elem) {
     let input = document.getElementById('amountOfCards');
     let amountOfCards = parseInt(input.value); //pido valor entero 
     orderCards = [];
-    
+
     /* Estructura de carta */
     for (let i = 0; i < amountOfCards; i++) {
         let randomNumber = Math.floor(Math.random() * numbers.length);
         let randomSuit = Math.floor(Math.random() * suits.length);
-        
+
         /* Div-class-card */
         let card = document.createElement('div');
         card.classList.add('card');
@@ -40,7 +29,7 @@ function createCards(elem) {
         let topSuit = document.createElement('div');
         topSuit.classList.add('topSuit');
         topSuit.innerHTML = suits[randomSuit];
-        
+
         /* parte media carta */
         //div class middleNumber
         let middleNumber = document.createElement('div');
@@ -70,7 +59,7 @@ function createCards(elem) {
         card.appendChild(middleNumber);
         card.appendChild(bottonSuit);
         elem.appendChild(card);
-        
+
         /* cuando cambia de valor el numero cambia a numero entero */
         let cardContent = {
             number: parseInt(changeValue(middleNumber.innerHTML)),
@@ -81,84 +70,58 @@ function createCards(elem) {
     }
 }
 
+
+/* inversor de valores de cartas A, J, Q, K */
+function changeValue(valor) {
+    switch (valor) {
+        case 1: return "A";
+        case 11: return "J";
+        case 12: return "Q";
+        case 13: return "K";
+        default: return valor;
+    }
+}
+
 /* DRAW button estructura*/
+
 btnDraw.addEventListener('click', (e) => {
+
     const cardDeck = document.querySelector('#cardDeck');
     cardDeck.innerHTML = "";
     createCards(cardDeck);
-
     let sortDeck = document.getElementById('sortDeck');
     sortDeck.innerHTML = "";
 });
 
 /* Sort button estructura*/
-/* btnSort.addEventListener('click' (e) => {
-    let sortDeck = document.getElementById('sortDeck');
-    sortDeck.innerHTML = ""; */
+btnSort.addEventListener("click", (e) => {
+    let sortDeck = document.getElementById("sortDeck");
+    sortDeck.innerHTML = "";
 
     /* bubble sort loop */
-    /* for (let j = orderCards.length - 1; j > 0; j++) {
+    for (let j = orderCards.length - 1; j > 0; j--) {
         for (let k = 0; k < j; k++) {
-            if (orderCards[k].number > orderCards[k+1].number){
+            /* condicional para ordenar cartas */
+            if (orderCards[k].number > orderCards[k + 1].number) {
                 let aux = orderCards[k];
+                orderCards[k] = orderCards[k + 1];
+                orderCards[k + 1] = aux;
+                /* create div with class lines inside the loop to put cards in lines*/
+                let firstStep = document.createElement('div');
+                firstStep.classList.add('lines');
+                sortDeck.appendChild(firstStep);
+                var lineBar = sortDeck.childElementCount;
+                firstStep.innerHTML = lineBar;
 
-              }
-             }
-         }
-    }
-
-}); */
-
-
-let sortArray = (arr) => {
-    let wall = arr.length - 1;
-    let numberOfTry = 0;
-    while (wall > 0) {
-        let index = 0;
-
-        while (index < wall) {
-
-            let log = document.createElement("p");
-            log.innerHTML = numberOfTry;
-            sortDeck.appendChild(log)
-            for (let j = 0; j < arr.length; j++) {
-                createCard(arr[j], sortDeck)
-
+                for (let h = 0; h < orderCards.length; h++) {
+                    /* create div with class newCard giving the existance of the cards in the Sort Loop */
+                    // with the same characteristic as the class .card to be the same printed inside
+                    let newCard = document.createElement('div');
+                    newCard.classList.add('newCard');
+                    newCard.innerHTML = orderCards[h].html;
+                    firstStep.appendChild(newCard);
+                }
             }
-
-            if (arr[index].number > arr[index + 1].number) {
-                let aux = arr[index].number;
-                arr[index].number = arr[index + 1].number;
-                arr[index + 1].number = aux;
-            }
-
-            index++
-            numberOfTry++
         }
-
-        wall--;
     }
-    return arr
-}
-
-let sortCards = () => {
-    let newCardSet = sortArray(cardsToSort);
-
-    if (newCardSet.length > 0) {
-
-        let cardTotal = document.querySelectorAll("#cardDeck > .card");
-        cardTotal.forEach(el => {
-            el.remove()
-        })
-
-        for (let i = 0; i < newCardSet.length; i++) {
-            createCard(newCardSet[i], cardDeck);
-        }
-
-
-
-    } else {
-        alert("No hay cartas para ordenar. Por favor presiona el boton de repartir")
-    }
-
-} 
+});
