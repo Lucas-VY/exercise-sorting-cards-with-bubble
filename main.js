80a5024f38a96083d921fc280aad71a63f5f349c
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-27 18:43:36 -0300

    resultado final

--- main.js
+++ main.js
@@ -74,10 +74,10 @@ function createCards(elem) {
 /* inversor de valores de cartas A, J, Q, K */
 function changeValue(valor) {
     switch (valor) {
-        case 1: return "A";
-        case 11: return "J";
-        case 12: return "Q";
-        case 13: return "K";
+        case '1': return "A";
+        case '11': return "J";
+        case '12': return "Q";
+        case '13': return "K";
         default: return valor;
     }
 }
